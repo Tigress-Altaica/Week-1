17646feb6a82f85908178986dbb9dbc319edb657
Anneliese Braunegg <akb89@case.edu>
2021-01-23 18:30:46 -0500

    Added classes for Parts 2 and 3

---
+++ assignment1/src/main/java/csis2450/assignment1/Part2.java
@@ -0,0 +1,10 @@
+package csis2450.assignment1;
+
+public class Part2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment1/src/main/java/csis2450/assignment1/Part3.java
@@ -0,0 +1,10 @@
+package csis2450.assignment1;
+
+public class Part3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
